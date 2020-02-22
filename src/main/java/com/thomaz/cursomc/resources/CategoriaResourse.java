2dc8b21a3a1e3f4e43756f17dc12c28737714888
Matheus Thomaz <matheusthomazt@gmail.com>
2020-02-22 01:30:10 -0300

    Criadas classes Categoria e CategoriaResourse

---
+++ src/main/java/com/thomaz/cursomc/resources/CategoriaResourse.java
@@ -0,0 +1,30 @@
+package com.thomaz.cursomc.resources;
+
+import com.thomaz.cursomc.domain.Categoria;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResourse {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2,"Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
+
+    }
+
+
+}
